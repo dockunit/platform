161e526fffd8815ce1d8d7cc45b09152f8d593b1
Taylor Lovett <tlovett88@gmail.com>
2015-08-06 01:24:31 -0400

    Fix home text

--- components/Home.jsx
+++ components/Home.jsx
@@ -11,7 +11,7 @@ class Home extends React.Component {
 				<div className="jumbotron">
 					<div className="container">
 						<h1>Welcome to Dockunit.io</h1>
-						<p>Dockunit.io is a <strong>containered</strong> continuous integration service for running Dockunit test suites hosted on <strong>Github</strong>. The days of staggering launches are long gone. Automatically test the integrity of each change to your software as a part of your workflow. Quickly and effectively respond to issues with insightful reports on software issues.</p>
+						<p>Dockunit.io is a <strong>containerized</strong> continuous integration service for running Dockunit test suites hosted on <strong>Github</strong>. The days of staggering launches are long gone. Automatically test the integrity of each change to your software as a part of your workflow. Quickly and effectively respond to issues with insightful reports on software issues.</p>
 						<p>
 							<NavLink className="btn btn-primary btn-lg" role="button" routeName="register">Sign up »</NavLink>
 						</p>
