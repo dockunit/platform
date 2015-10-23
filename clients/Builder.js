737960b001eabf9b534d04009d2592153251ffbc
Taylor Lovett <tlovett88@gmail.com>
2015-10-23 14:28:32 -0400

    Debug node containers

--- clients/Builder.js
+++ clients/Builder.js
@@ -11,7 +11,6 @@ var Github = require('./Github');
 var constants = require('../constants');
 var Convert = require('ansi-to-html');
 
-debug = console.log;
 
 var Builder = function(user, project, buildId) {
 	var self = this;
